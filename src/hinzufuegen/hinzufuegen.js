9f7203b4cc393af321eed409f9498be6756e22b7
TheWorstAvenger <alexander.roman.knobloch@gmail.com>
2019-11-14 15:06:14 +0100

    Baureihe Listener hinzugefügt
    gibt logausgaben über modellwahl

--- src/hinzufuegen/hinzufuegen.js
+++ src/hinzufuegen/hinzufuegen.js
@@ -13,6 +13,35 @@ class HinzufuegenPage {
             console.log("KLICKMICH!!")
         });
 
+        hinzuPage.querySelector('#modell').addEventListener('change', function() {
+            var modellMenu = document.querySelector('#modell');
+            var modellIndex = modellMenu.selectedIndex;
+            var modellText = modellMenu.options[modellIndex].text
+            var  baureihe = document.querySelector('#baureihe');
+            console.log("Modell Changed!!" +" Modellindex: " +modellIndex + " ModellText: " +modellText);
+            switch (modellText) {
+              case "911":
+                  console.log("911 ausgewählt")
+                break;
+
+              case "718":
+                  console.log("718 ausgewählt")
+                break;
+
+              case "Cayenne":
+                  console.log("Cayenne ausgewählt")
+                break;
+
+               case "Macan":
+                  console.log("Macan ausgewählt")
+                break;
+
+              case "Panamera":
+                  console.log("Panamera ausgewählt")
+                break;
+            }
+        });
+
         return this._createContentObject(hinzuPage);
     }
 
@@ -34,6 +63,8 @@ class HinzufuegenPage {
     get title() {
         return "hinzufügen";
     }
+
+
 }
 
 export default HinzufuegenPage;
