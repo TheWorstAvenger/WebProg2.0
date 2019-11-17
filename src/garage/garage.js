fc5a457a9666f82cf87fe81711d5f309c48a2bb4
TomEckert00 <toe00@web.de>
2019-11-17 19:42:12 +0100

    erster versuch beim mechanismus

--- src/garage/garage.js
+++ src/garage/garage.js
@@ -11,6 +11,24 @@ class GaragePage {
     async onShow() {
         let garagePage = await this._importStartPageHtml();
 
+        let carCount = localStorage.length;
+        console.log(carCount);
+        let carCount911 = JSON.parse(localStorage.getItem("911")).length;
+        console.log("911: "+carCount911);
+
+        var tbody = document.querySelector("#autoliste ul");
+        var temp = document.querySelector("#neuesAuto");
+
+        if('content' in document.createElement('template')&&tbody&&temp){
+          tbody.innerHTML="";
+
+          tr = document.importNode(temp.content, true);
+          tbody.appendChild(tr);
+        }
+
+
+
+
         garagePage.querySelector('.localstorageloeschen').addEventListener('click', () => {
           console.log("ALLES LÖSCHEN!!");
           localStorage.clear();
